bf9b91ed86f44e62356f02eea0273069bf5bc2bd
asqw887 <asqw887@gmail.com>
2022-04-07 19:42:00 +0900

    FEAT: 카드 섹션별 분리 기능 구현

--- TodoList/TodoList/Model/Board.swift
+++ TodoList/TodoList/Model/Board.swift
@@ -9,12 +9,17 @@ import Foundation
 
 
 class Board{
-//    private var cards: [Card]?
-    private var todoCards: [Card]?
-    private var doginCards: [Card]?
-    private var doneCards: [Card]?
+    private(set) var todoCards: [Card]?
+    private(set) var doginCards: [Card]?
+    private(set) var doneCards: [Card]?
     
-    
+    func divideCard() {
+        guard let cards = URLManager.requestGet(url: "") else { return }
+        todoCards = cards.filter{ $0.section == Card.State.todo }
+        doginCards = cards.filter{ $0.section == Card.State.doing }
+        doneCards = cards.filter{ $0.section == Card.State.done }
+    }
+
     
     
 }
