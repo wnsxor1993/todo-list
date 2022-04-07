44811074f185ed246c17fa27bcebd5f2cbb8c764
asqw887 <asqw887@gmail.com>
2022-04-07 18:07:54 +0900

    FEAT: Board Model 구현

--- TodoList/TodoList/Model/Board.swift
+++ TodoList/TodoList/Model/Board.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+
+class Board{
+//    private var cards: [Card]?
+    private var todoCards: [Card]?
+    private var doginCards: [Card]?
+    private var doneCards: [Card]?
+    
+    
+    
+    
+}
